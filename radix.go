2d181957295f0406db88be7b3c8411717609dd98
Tom Bevan <tom.bevan@r3labs.io>
2019-04-30 18:38:21 +0100

    stashing changes

--- radix.go
+++ radix.go
@@ -19,25 +19,25 @@ func New() *Radix {
 
 // Insert or update a keypair
 func (r *Radix) Insert(key []byte, value interface{}) {
-	parent, pos, dv := r.findInsertionPoint(key)
-
-	parent.print()
-	fmt.Println(pos)
-	fmt.Println(dv)
+	parent, node, pos, dv := r.findInsertionPoint(key)
 
 	switch {
+
+	/*
+		case len(node.prefix) > 0 && len(node.prefix) > dv:
+			r.splitNode(key, parent, node, pos, dv)
+		case pos == len(key):
+			r.updateNode(key, parent, node, pos, dv)
+		case n.HasPrefix() && dv == n.PrefixSize() || i < len(key):
+			r.insertNode(key, parent, node, pos, db)
+	*/
+
 	case pos == len(key):
-		// update!
-		fmt.Println("update")
+		r.updateNode(key, value, parent, node, pos, dv)
 	case pos < len(key) && dv == 0:
-		// insert simple
-		fmt.Println("insert")
-		parent.setNext(key[pos], &Node{
-			prefix: key[pos+1:],
-			value:  key,
-		})
+		r.insertNode(key, value, parent, node, pos, dv)
 	default:
-		fmt.Println("UGH")
+		r.splitNode(key, value, parent, node, pos, dv)
 	}
 }
 
@@ -105,33 +105,73 @@ func (r *Radix) graphviz(gvoutput *[]string, gvzc *int, previous string, n *Node
 	}
 }
 
-func (r *Radix) findInsertionPoint(key []byte) (*Node, int, int) {
-	var i, dv int
-	var parent *Node
+func (r *Radix) findInsertionPoint(key []byte) (*Node, *Node, int, int) {
+	var pos, dv int
+	var node, parent *Node
 
-	n := r.root
+	node = r.root
 
-	for n.next(key[i]) != nil {
-		parent = n
+	for node.next(key[pos]) != nil {
+		parent = node
 
-		n = n.next(key[i])
-		i++
+		node = node.next(key[pos])
+		pos++
 
-		if len(n.prefix) > 0 {
-			dv = divergence(n.prefix, key[i:])
+		if len(node.prefix) > 0 {
+			dv = divergence(node.prefix, key[pos:])
 
-			if len(n.prefix) > dv {
-				return parent, i, dv
+			if len(node.prefix) > dv {
+				return parent, node, pos, dv
 			}
 
-			i = i + dv
+			pos = pos + dv
 		}
 
 		// if we've found the key, return its parent node so it can be pointed to the new node
-		if i == len(key) {
-			return parent, i, dv
+		if pos == len(key) {
+			return parent, node, pos, dv
 		}
 	}
 
-	return n, i, dv
+	return node, nil, pos, dv
+}
+
+func (r *Radix) insertNode(key []byte, value interface{}, parent, node *Node, pos, dv int) {
+	fmt.Println("insert")
+
+	parent.setNext(key[pos], &Node{
+		prefix: key[pos+1:],
+		value:  value,
+	})
+}
+
+func (r *Radix) updateNode(key []byte, value interface{}, parent, node *Node, pos, dv int) {
+	fmt.Println("update")
+
+	parent.setNext(key[pos], &Node{
+		prefix: key[pos+1:],
+		value:  value,
+		edges:  node.edges,
+	})
+}
+
+func (r *Radix) splitNode(key []byte, value interface{}, parent, node *Node, pos, dv int) {
+	fmt.Println("split")
+
+	parent.print()
+	if node != nil {
+		node.print()
+	}
+
+	fmt.Println("create node", string(key[pos]), "->", string(key[pos+1:pos+dv]))
+	fmt.Println("create node", string(node.prefix[dv]), "->", string(node.prefix[dv+1:]))
+
+	/*
+		n1 := &Node{
+			prefix:
+		}
+	*/
+
+	fmt.Println(pos)
+	fmt.Println(dv)
 }
